ac37c8eb7ce2ce3d7d48b3d1dbf5bfad19ebf178
dev-yoondam <dev.yoondam@gmail.com>
2023-07-10 18:09:11 +0900

    담 7/10 마이페이지 수정

---
+++ WebContent/mypage/js/mypage.js
@@ -0,0 +1,9 @@
+document.getElementById("feed").onclick = function() {mouseClick()};
+document.getElementById("trade").onclick = function() {mouseClick()};
+document.getElementById("with").onclick = function() {mouseClick()};
+document.getElementById("scrap").onclick = function() {mouseClick()};
+
+function mouseClick() {
+alert("aa");
+
+};
