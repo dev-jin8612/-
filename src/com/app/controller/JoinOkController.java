03f0b447ad1f84eeed863428f541e8f46e0cdd38
dev-jin8612 <workspace8612@gmail.com>
2023-07-15 16:09:44 +0900

    검색창 복구

---
+++ src/com/app/controller/JoinOkController.java
@@ -0,0 +1,37 @@
+package com.app.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.app.Action;
+import com.app.Result;
+import com.app.dao.memberDAO;
+import com.app.vo.memberDTO;
+
+public class JoinOkController implements Action {
+	@Override
+	public Result execute(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+		memberDAO memberDAO = new memberDAO();
+		memberDTO memberDTO = new memberDTO();
+		Result result = new Result();
+
+//	사용자가 화면에서 입력한 값을 전부 받아온다.
+//	화면에서 입력한 값은 모두 memberDTO객체에 담아준다.
+		memberDTO.setMemberId(req.getParameter("memberId"));
+		memberDTO.setMemberPassword(req.getParameter("memberPassword"));
+		memberDTO.setMemberName(req.getParameter("memberName"));
+		memberDTO.setMemberNick(req.getParameter("memberNick"));
+		memberDTO.setMemberHP(req.getParameter("memberHP"));
+		memberDTO.setMemberImg(req.getParameter("memberImg"));
+		
+		memberDAO.insert(memberDTO);
+
+		result.setRedirect(true);
+		result.setPath(req.getContextPath() + "/login.member");
+
+		return result;
+	}
+}
